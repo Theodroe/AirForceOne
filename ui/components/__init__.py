a9114c023b3c1bfa61812fd1ba3fcdaa46c4ec62
AnDante <131837191+Theodroe@users.noreply.github.com>
2026-04-03 03:44:12 +0900

    Delete ui directory

--- ui/components/__init__.py
+++
@@ -1,9 +0,0 @@
-
-from .charts import (
-    draw_bar_chart,
-    draw_heatmap_chart,
-    draw_line_chart,
-    draw_multi_line_chart,
-    draw_annual_day_heatmap,
-    draw_daily_reference_chart,
-)
